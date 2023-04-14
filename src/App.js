5f02103c1884bdf935c899dc84924d779dba27bc
Arpana998 <arpanachavan998@gmail.com>
2023-04-14 01:12:50 -0700

    access-denied-to-store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import About from "./Components/Pages/AboutPage/About";
 import Home from "./Components/Pages/HomePage/Home";
 import Store from "./Components/Pages/Store"
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavbarHeader from "./Components/Pages/StorePage/Navbar";
 import ContactUsPage from "./Components/Pages/ContactPage/ContactUsPage";
 import ProductSpecificPage from "./Components/Pages/StorePage/ProductSpecificPage";
 import Login from "./Components/Pages/LoginPage/Login";
+import { useContext } from "react";
+import AuthContext from "./Store/AuthContext";
 
 
 
 
 let App = () => {
 
-  
+  const authCtx = useContext(AuthContext);
   
   return(
     <>
@@ -21,13 +23,15 @@ let App = () => {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />}/>
-        <Route path="/store" element={<Store />}/>
+        {authCtx.isLoggedin && <Route path="/store" element={<Store />}/>}
         <Route path="/contactus" element={<ContactUsPage />}/>
         <Route path="/store/product/:selectedItemId" element={<ProductSpecificPage />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="*" element = {<Navigate to="/login" />} />
       </Routes>
       </>
-   
+   //e <Route element={<Navigate to="/" />} /> is the new
+   //<Route path="*" element={<Navigate to="/" />} />,
   )
 
 }
